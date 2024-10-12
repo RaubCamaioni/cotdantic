5857d63b78477823740a4bc563a7bae3ca624afd
Raub Camaioni <raubcameo@gmail.com>
2024-10-12 14:57:06 -0400

    first release

--- src/cotdantic/__version__.py
+++ src/cotdantic/__version__.py
@@ -1,10 +1,1 @@
-import toml
-
-
-def get_version():
-	with open('pyproject.toml', 'r') as f:
-		data = toml.load(f)
-		return data['project']['version']
-
-
-__version__ = get_version()
+__version__ = '1.0.1'
